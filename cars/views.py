f2b44a4dc2bbe6d59f101f43f78bf97493c49895
Anailton Silva <francisco.anailton17@gmail.com>
2024-01-30 20:09:58 -0300

    deleting new_car

--- cars/views.py
+++ cars/views.py
@@ -16,19 +16,6 @@ class CarsView(View):
         return render(request, 'cars.html', {'cars': cars})
 
 
-def new_car_view(request):
-    if request.method == 'POST':
-        new_car_form = CarModelForm(request.POST, request.FILES)
-        if new_car_form.is_valid():
-            new_car_form.save()
-            return redirect('cars_list')
-
-    else:
-        new_car_form = CarModelForm()
-    return render(request, 'new_car.html',
-                  {'new_car_form': new_car_form})
-
-
 class NewCarView(View):
 
     def get(self, request):
