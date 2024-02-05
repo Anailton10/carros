f9dd11efa05bb37bdca3ad8ec2acdf4090949b25
Anailton Silva <francisco.anailton17@gmail.com>
2024-02-05 18:24:46 -0300

    Sobreescrevendo a CarsView com a ListView

--- cars/views.py
+++ cars/views.py
@@ -2,18 +2,22 @@ from django.shortcuts import render, redirect
 from .models import Car
 from cars.forms import CarModelForm
 from django.views import View
+from django.views.generic import ListView
 
 
-class CarsView(View):
+class CarsView(ListView):
+    model = Car
+    template_name = 'cars.html'
+    context_object_name = 'cars'
 
-    def get(self, request):
-        cars = Car.objects.all().order_by('-id')
-        search = request.GET.get('search')
-
+    def get_queryset(self):
+        # cars = Car.objects.all().order_by('-id')
+        cars = super().get_queryset().order_by('-id')
+        search = self.request.GET.get('search')
         if search:
             cars = cars.filter(model__icontains=search)
 
-        return render(request, 'cars.html', {'cars': cars})
+        return cars
 
 
 class NewCarView(View):
@@ -28,3 +32,5 @@ class NewCarView(View):
         if new_car_form.is_valid():
             new_car_form.save()
             return redirect('cars_list')
+        return render(request, 'new_car.html',
+                      {'new_car_form': new_car_form})
