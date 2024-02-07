69f751bb4826b7446bb60cefb34df1520af8dfb0
Anailton Silva <francisco.anailton17@gmail.com>
2024-02-07 15:45:01 -0300

    feat: Adicionando a Class CarDetailView para os detalhes de cada carro

--- app/urls.py
+++ app/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from cars.views import CarsView, NewCarCreateView
+from cars.views import CarsView, NewCarCreateView, CarDetailView
 from accounts.views import register_view, login_view, logout_view
 
 urlpatterns = [
@@ -28,4 +28,5 @@ urlpatterns = [
     path('logout/', logout_view, name='logout'),
     path('cars/', CarsView.as_view(), name='cars_list'),
     path('new_car/', NewCarCreateView.as_view(), name='new_car'),
+    path('car/<int:pk>/', CarDetailView.as_view(), name='car_detail')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
