c102ea8f811c03e0febb2f1681da7db94511dc04
Anailton Silva <francisco.anailton17@gmail.com>
2024-02-07 16:38:22 -0300

    feat: Adicionando o Upadate para atualização dos carros

--- cars/views.py
+++ cars/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView, CreateView, DetailView
+from django.views.generic import ListView, CreateView, DetailView, UpdateView
 from cars.forms import CarModelForm
 from .models import Car
 
@@ -28,3 +28,10 @@ class NewCarCreateView(CreateView):
 class CarDetailView(DetailView):
     model = Car
     template_name = 'car_detail.html'
+
+
+class CarUpdateView(UpdateView):
+    model = Car
+    form_class = CarModelForm
+    template_name = 'car_update.html'
+    success_url = '/cars/'
