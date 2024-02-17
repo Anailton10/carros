d1fbb0a8021f60693793468777fc066f50a70311
Anailton Silva <francisco.anailton17@gmail.com>
2024-02-17 16:05:18 -0300

    feat: signal para criação da bio

--- cars/signals.py
+++ cars/signals.py
@@ -1,5 +1,5 @@
 from django.db.models import Sum
-from django.db.models.signals import post_delete, post_save  # noqa: E501
+from django.db.models.signals import post_delete, post_save, pre_save  # noqa: E501
 from django.dispatch import receiver
 
 from cars.models import Car, CarInventory
@@ -12,6 +12,12 @@ def cars_inventory_update():
     cars_count = Car.objects.all().count()
     cars_value = Car.objects.aggregate(total_value=Sum("value"))["total_value"]
     CarInventory.objects.create(cars_count=cars_count, cars_value=cars_value)
+
+
+@receiver(pre_save, sender=Car)
+def car_pre_save(sender, instance, **kwargs):
+    if not instance.bio:
+        instance.bio = "Bio criada via signals"
 
 
 @receiver(post_save, sender=Car)
