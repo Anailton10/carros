620bfb9832f63a1de0204cf3a65f48d684a77bb5
Anailton Silva <francisco.anailton17@gmail.com>
2024-02-17 12:59:57 -0300

    feat: add campo bio

--- cars/models.py
+++ cars/models.py
@@ -13,12 +13,14 @@ class Car(models.Model):
     id = models.AutoField(primary_key=True)
     model = models.CharField(max_length=200)
     brand = models.ForeignKey(
-        Brand, on_delete=models.PROTECT, related_name='car_brand')
+        Brand, on_delete=models.PROTECT, related_name="car_brand"
+    )  # noqa: E501
     factory_year = models.IntegerField(blank=True, null=True)
     model_year = models.IntegerField(blank=True, null=True)
     plate = models.CharField(max_length=10, blank=True, null=True)
     value = models.FloatField(blank=True, null=True)
-    photo = models.ImageField(upload_to='cars/', blank=True, null=True)
+    photo = models.ImageField(upload_to="cars/", blank=True, null=True)
+    bio = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.model
@@ -30,7 +32,7 @@ class CarInventory(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        ordering = ['-created_at']
+        ordering = ["-created_at"]
 
     def __str__(self):
         return f" {self.cars_count} - {self.cars_value}"
